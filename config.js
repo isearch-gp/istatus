ca936bcf8645b7affd8c96ec5e20168e8111b881
mkobar <mkobar@rkosecurity.com>
2020-03-02 18:21:24 -0500

    added api key

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var monitors = [
-    "apikey1",  //Place your first Key Here
+    "ur879801-9720ecbd38b587e98430dfba",  //Place your first Key Here
     "apikey2", //Place your next key here! <- Keep duplicating this line for more than 2 keys!
 
     
 ];
-var title = 'My Project Status Page'; //Page <title> and heading at top of page
+var title = 'iSearch Status Page'; //Page <title> and heading at top of page
 var refreshtime = 60; //How often to refresh in seconds
 var footermessage = ''; //HTML to go in footer
