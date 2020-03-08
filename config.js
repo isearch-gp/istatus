8740d501334d531ba53ff88a44611a7aa2ab75cb
mkobar <mkobar@rkosecurity.com>
2020-03-07 19:16:04 -0500

    added maps

--- config.js
+++ config.js
@@ -3,7 +3,9 @@ var monitors = [
     "m784437563-3180dfa269b28789a8f786f6", //Place your next key here! <- app demo
     "m784438298-02f8d06be96a9c9e468770d4", //Place your next key here! <- API GUI
     "m784438306-8c3c187da5a48781f6b4c6f4", //Place your next key here! <- API Google
-"m784438301-f480bb16afc2bdcd045fc3aa", //Place your next key here! <- API SERP
+    "m784438301-f480bb16afc2bdcd045fc3aa", //Place your next key here! <- API SERP
+    "m784469888-bf0e77a463ae9e95f94aee39", // Map proto 1
+    "m784469890-5fce49f43d89cdc79d8545cb"  // Map proto 2
     
 ];
 var title = 'iSearch Status Page'; //Page <title> and heading at top of page
